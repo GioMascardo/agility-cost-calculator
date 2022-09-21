c1258c0c3877e0eef23014f058cb0c2844e2e6c8
Gio Mascardo <mascardogio@gmail.com>
2022-09-22 00:02:00 +0800

    followed sendgrid integration guide

--- api/formSubmit.js
+++ api/formSubmit.js
@@ -1,7 +1,7 @@
 import sgMail from "@sendgrid/mail";
 
-console.log("before setApiKey", process.env.VITE_SG_API_KEY);
-sgMail.setApiKey(process.env.VITE_SG_API_KEY);
+console.log("before setApiKey", process.env.SENDGRID_API_KEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default async function handler(request, response) {
   const body = JSON.parse(request.body);
