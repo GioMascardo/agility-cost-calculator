4e0698a0b2aeb6ea2dcd50ad1c245159b40ab319
Gio Mascardo <mascardogio@gmail.com>
2022-09-21 23:34:19 +0800

    filled in the handler function

--- api/formSubmit.js
+++ api/formSubmit.js
@@ -4,40 +4,21 @@ sgMail.setApiKey(process.env.VITE_SG_API_KEY);
 console.log("setApiKey");
 
 export default async function handler(request, response) {
-  // const body = JSON.parse(request.body);
+  const body = JSON.parse(request.body);
 
-  // const message = `
-  //   Good day Mr./Ms. ${
-  //     body.lastName
-  //   },\r\n\r\nHere's a summary of your team using our cost calculator:\r\n
-  //   ${body.dashboardSummary.forEach((row) => {
-  //     let {
-  //       role,
-  //       staffRequired,
-  //       experienceLevel,
-  //       hireWithAgility,
-  //       hireOnshore,
-  //       yourSavings,
-  //     } = row;
-  //     return `- Role: ${role}, Number of ${role}${
-  //       staffRequired > 1 ? "s" : ""
-  //     }: ${staffRequired},
-  //     Experience Level: ${experienceLevel},
-  //     Hire With Us: ${hireWithAgility},
-  //     Hire Onshore: ${hireOnshore},
-  //     Your Savings: ${yourSavings}\r\n`;
-  //   })}
-  // `;
+  const message = `
+    Good day Mr./Ms. ${body.lastName}, 
+    this is a test email.
+  `;
 
-  // const data = {
-  //   to: `${body.email}`,
-  //   from: "mascardogio@gmail.com",
-  //   subject: "Agility Cost Calculator Summary",
-  //   text: message,
-  //   html: message.replace(/\r\n/g, "<br>"),
-  // };
+  const data = {
+    to: `${body.email}`,
+    from: "mascardogio@gmail.com",
+    subject: "Agility Cost Calculator Summary",
+    text: message,
+  };
 
-  // sgMail.send(data);
+  sgMail.send(data);
 
   return response.status(200).json({ res: "test response" });
 }
