952917609935be81765cb098d7c71eb29d360688
Gio Mascardo <mascardogio@gmail.com>
2022-09-23 21:49:45 +0800

    test sample

--- api/formSubmit.js
+++ api/formSubmit.js
@@ -1,50 +1,59 @@
-// import sgMail from "@sendgrid/mail";
+import { google } from "googleapis";
+import nodemailer from "nodemailer";
 
-// console.log("before setApiKey", process.env.SENDGRID_API_KEY);
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-export default function handler(request, response) {
-  const body = JSON.parse(request.body);
-
-  const message = `
-    Good day Mr./Ms. ${body.lastName}, 
-    this is a test email.
-  `;
-
-  const data = {
-    to: `${body.email}`,
-    from: "mascardogio@gmail.com",
-    subject: "Agility Cost Calculator Summary",
-    text: message,
-  };
-
-  request("https://api.sendgrid.com/v3/mail/send", {
-    method: "post",
+const generateConfig = (url, accessToken) => {
+  return {
+    method: "get",
+    url: url,
     headers: {
-      Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`,
-      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken} `,
+      "Content-type": "application/json",
     },
-    body: data,
-  })
-    .then((data) => {
-      console.log("Success:", data);
-      return response.status(200);
-    })
-    .catch((error) => {
-      console.log("Error:", error);
-      return response.status(500);
+  };
+};
+
+const auth = {
+  type: "OAuth2",
+  user: "sid.cd.varma@gmail.com",
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  refreshToken: process.env.REFRESH_TOKEN,
+};
+
+const mailoptions = {
+  from: "inquiry@agilitystaffingservices.com",
+  to: "gl.mascardo@gmail.com",
+  subject: "Gmail API NodeJS",
+};
+
+export default async function handler(request, response) {
+  const oAuth2Client = new google.auth.oAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    process.env.REDIRECT_URL
+  );
+
+  oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
+
+  try {
+    const accessToken = await oAuth2Client.getAccessToken();
+    const transport = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        auth,
+        accessToken: accessToken,
+      },
     });
 
-  // sgMail
-  //   .send(data)
-  //   .then(() => {
-  //     console.log("Email sent");
-  //     console.log(data);
-  //   })
-  //   .catch((error) => {
-  //     console.error(error);
-  //     console.log(data);
-  //   });
+    const mailOptions = {
+      mailoptions,
+      text: "The Gmail API with NodeJS works",
+    };
 
-  // return response.status(200).json({ res: "test response" });
+    const result = await transport.sendMail(mailOptions);
+    response.send(result);
+  } catch (error) {
+    console.error(error);
+    response.send(error);
+  }
 }
